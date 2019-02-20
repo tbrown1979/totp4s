2b20b2b44b089d97ab2844409858e5c645db7762
tjohnson <trent.johnson@banno.com>
2019-02-20 09:55:21 -0600

    Adding a specific error for invalid totp code format

--- core/src/main/scala/io/tbrown/totp4s/models.scala
+++ core/src/main/scala/io/tbrown/totp4s/models.scala
@@ -5,3 +5,5 @@ case class Secret(value: String) extends AnyVal //secret used to generate the TO
 case class Window(value: Int) extends AnyVal //How many timesteps the TOTP will be checked against for validation
 case class TimeStep(value: Long) extends AnyVal //seconds range that the TOTP is valid for
 case class TimeSteps(value: Long) extends AnyVal // number of TimeSteps for the current UNIX time, this divided by TimeStep
+
+case class InvalidCodeFormat(msg: String) extends Throwable
